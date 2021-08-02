c9c581f75ee9d2489643c9140abb1e5352152cb6
Szymon Steczek <szymon.steczek42@gmail.com>
2021-08-02 21:32:14 +0200

    Custom Queue defined in C - Python wrapper example

---
+++ cython/c_custom_script_test/test_queue.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import time
+
+import queue
+
+Q = queue.Queue()
+
+Q.append(10)
+Q.append(20)
+print(Q.peek())
+print(Q.pop())
+print(Q.pop())
+try:
+    print(Q.pop())
+except IndexError as e:
+    print(f"Error message: {e}")
+
+i = 10000
+
+values = range(1000)
+
+start_time = time.time()
+
+Q.extend(values)
+end_time = time.time()  - start_time
+
+print("Adding {} items took {:1.3f} msecs.".format(i, 1000 * end_time))
+
+for i in range(41):
+    Q.pop()
+
+Q.pop()
+print("The answer is: ")
+print(Q.pop())
